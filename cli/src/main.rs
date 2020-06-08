5b95083fdc7b944785430362343bfcda13102fb4
YoEight <yo.eight@gmail.com>
2020-06-08 17:43:00 +0200

    Add access CLI parsing example.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,40 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    Access(Access),
+}
+
+#[derive(StructOpt, Debug)]
+struct Access {
+    #[structopt(subcommand)]
+    access_command: AccessCommand,
+}
+
+#[derive(StructOpt, Debug)]
+enum AccessCommand {
+    User(User),
+}
+
+#[derive(StructOpt, Debug)]
+struct User {
+    #[structopt(subcommand)]
+    user_command: UserCommand,
+}
+
+#[derive(StructOpt, Debug)]
+enum UserCommand {
+    List,
+    Revoke,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("> {:?}", opt);
 }
