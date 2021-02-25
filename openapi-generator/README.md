6af1ee55f3f9d30b4fbe3529c3f9872f4ec25ce4
Tim Simpson <timsimpson4@gmail.com>
2021-02-25 13:10:12 -0600

    more stuff

---
+++ openapi-generator/README.md
@@ -0,0 +1,8 @@
+# How to Generate the Clients
+
+1. Make sure the following tools are available on your path:
+
+    [openapi-generator](api-generator.tech/)
+    [yq](https://github.com/mikefarah/yq)
+
+2. Run `generate.sh`
--- openapi-generator/generate.sh
+++ openapi-generator/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 # rm -r ../../client/
 mkdir -p ../../client/rust
@@ -9,19 +9,44 @@ mkdir -p ../../client/rust
 
 export IMPORT_MAPPINGS=
 
-echo 'Creating OpenAPI Generator style client...'
-openapi-generator generate -i resources.yaml -g rust -o ../../client/rust    --import-mappings=chrono::DateTime=chrono::DateTime  '--type-mappings=DateTime=chrono::DateTime<chrono::Utc>'
+# echo 'Creating OpenAPI Generator style client...'
+# openapi-generator generate -i resources.yaml -g rust -o ../../client/rust    --import-mappings=chrono::DateTime=chrono::DateTime  '--type-mappings=DateTime=chrono::DateTime<chrono::Utc>'
 
-echo 'Creating ESC style client...'
-mkdir -p ../../client/rust-esc/src
-echo 'Copying over static files...'
-cp -r templates/rust-esc/static/* ../../client/rust-esc/
-echo 'Running openapi-generator...'
-# openapi-generator generate -i resources.yaml -g rust --package-name esc-api -o ../../client/rust-esc/api    '--import-mappings=chrono::DateTime=chrono::DateTime,OrgId=crate::OrgId'  '--type-mappings=DateTime=chrono::DateTime<chrono::Utc>,orgid=OrgId' -t templates/rust-esc/mustache
-openapi-generator batch --includes-base-dir generate generate.yaml
-echo 'Building...'
-pushd ../../client/rust-esc/api
-RUSTFLAGS="-Z macro-backtrace" cargo build
+mkdir -p target
+
+yq 
+
+# readonly apis=('resources' 'mesdb')
+readonly apis=('resources')
+readonly clients=('rust-esc' 'rust' 'go')
+
+for api in "${apis[@]}"; do
+    for client in "${clients[@]}"; do
+        echo "generating ${api} :: ${client} ..."
+        echo "inputSpec: ../specs/${api}.yaml" > target/input.yaml
+        echo "outputDir: ../../client/${client}/${api}" > target/output.yaml
+        # echo "outputDir: ../../client/${client}/${api}" > target/output.yaml
+        # echo "../specs/${api}.yaml"
+        if [[ "${client}" == "rust-esc" ]]; then 
+            mkdir -p "../../client/${client}/${api}"
+            cp -r templates/rust-esc/static/api/* "../../client/${client}/${api}"
+        fi
+        openapi-generator batch --includes-base-dir configs "configs/${client}.yaml"
+    done
+done
+
+pushd ../../client/rust-esc/resources
+cargo build
+
+# mkdir -p ../../client/rust-esc/src
+# echo 'Copying over static files...'
+# cp -r templates/rust-esc/static/* ../../client/rust-esc/
+# echo 'Running openapi-generator...'
+
+# openapi-generator batch --includes-base-dir configs configs/rust-esc.yaml
+# echo 'Building...'
+# pushd ../../client/rust-esc/api
+# RUSTFLAGS="-Z macro-backtrace" cargo build
 
 # openapi-generator generate -i resources.yaml -g rust -o client/rust --additional-properties=--packageName=escg
 # openapi-generator generate -i resources.yaml -g go -o ../../client/go
