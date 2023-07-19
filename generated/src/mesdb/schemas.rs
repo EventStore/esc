2a109749ab71c366bacb821a3d7d7297afcbfc96
Artur Gadelshin <artur.gadelshin@eventstore.com>
2023-07-19 12:43:18 +0000

    add protected field

--- generated/src/mesdb/schemas.rs
+++ generated/src/mesdb/schemas.rs
@@ -6,7 +6,7 @@ use chrono::DateTime;
 use chrono::Utc;
 use std::collections::HashMap;
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Backup {
     pub available_node_count: i32,
@@ -26,7 +26,7 @@ pub struct Backup {
     pub status: String,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Cluster {
     pub can_expand_disk: bool,
@@ -50,24 +50,90 @@ pub struct Cluster {
     pub region: String,
     pub server_version: String,
     pub server_version_tag: String,
-    pub status: String,
+    pub status: ClusterStatus,
     pub topology: Topology,
+    pub protected: bool,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ClusterStatus {
+    #[serde(rename = "provisioning")]
+    Provisioning,
+    #[serde(rename = "disks available")]
+    DisksAvailable,
+    #[serde(rename = "expanding disks")]
+    ExpandingDisks,
+    #[serde(rename = "restarting")]
+    Restarting,
+    #[serde(rename = "available")]
+    Available,
+    #[serde(rename = "defunct")]
+    Defunct,
+    #[serde(rename = "inconsistent")]
+    Inconsistent,
+    #[serde(rename = "deleting instances")]
+    DeletingInstances,
+    #[serde(rename = "instances deleted")]
+    InstancesDeleted,
+    #[serde(rename = "deleting disks")]
+    DeletingDisks,
+    #[serde(rename = "deleted")]
+    Deleted,
+}
+impl std::fmt::Display for ClusterStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ClusterStatus::Provisioning => write!(f, "provisioning"),
+            ClusterStatus::DisksAvailable => write!(f, "disks available"),
+            ClusterStatus::ExpandingDisks => write!(f, "expanding disks"),
+            ClusterStatus::Restarting => write!(f, "restarting"),
+            ClusterStatus::Available => write!(f, "available"),
+            ClusterStatus::Defunct => write!(f, "defunct"),
+            ClusterStatus::Inconsistent => write!(f, "inconsistent"),
+            ClusterStatus::DeletingInstances => write!(f, "deleting instances"),
+            ClusterStatus::InstancesDeleted => write!(f, "instances deleted"),
+            ClusterStatus::DeletingDisks => write!(f, "deleting disks"),
+            ClusterStatus::Deleted => write!(f, "deleted"),
+        }
+    }
+}
+impl std::cmp::PartialEq<&str> for ClusterStatus {
+    fn eq(&self, other: &&str) -> bool {
+        match self {
+            ClusterStatus::Provisioning => *other == "provisioning",
+            ClusterStatus::DisksAvailable => *other == "disks available",
+            ClusterStatus::ExpandingDisks => *other == "expanding disks",
+            ClusterStatus::Restarting => *other == "restarting",
+            ClusterStatus::Available => *other == "available",
+            ClusterStatus::Defunct => *other == "defunct",
+            ClusterStatus::Inconsistent => *other == "inconsistent",
+            ClusterStatus::DeletingInstances => *other == "deleting instances",
+            ClusterStatus::InstancesDeleted => *other == "instances deleted",
+            ClusterStatus::DeletingDisks => *other == "deleting disks",
+            ClusterStatus::Deleted => *other == "deleted",
+        }
+    }
+}
+impl std::cmp::PartialEq<ClusterStatus> for &str {
+    fn eq(&self, other: &ClusterStatus) -> bool {
+        other == self
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateBackupRequest {
     pub description: String,
     pub source_cluster_id: ClusterId,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateBackupResponse {
     pub id: BackupId,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateClusterRequest {
     pub description: String,
@@ -86,15 +152,17 @@ pub struct CreateClusterRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub source_node_index: Option<i32>,
     pub topology: Topology,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub protected: Option<bool>,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateClusterResponse {
     pub id: ClusterId,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ExpandClusterDiskRequest {
     pub disk_size_gb: i32,
@@ -108,20 +176,20 @@ pub struct ExpandClusterDiskRequest {
 
 pub type Fields = HashMap<String, String>;
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GetBackupResponse {
     pub backup: Backup,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GetClusterResponse {
     pub cluster: Cluster,
 }
 
 /// The health of the database
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Health {
     #[serde(rename = "degraded")]
     Degraded,
@@ -145,21 +213,37 @@ impl std::fmt::Display for Health {
         }
     }
 }
+impl std::cmp::PartialEq<&str> for Health {
+    fn eq(&self, other: &&str) -> bool {
+        match self {
+            Health::Degraded => *other == "degraded",
+            Health::Down => *other == "down",
+            Health::HealthReportingError => *other == "health-reporting-error",
+            Health::Ok => *other == "ok",
+            Health::IssuesDetected => *other == "issues-detected",
+        }
+    }
+}
+impl std::cmp::PartialEq<Health> for &str {
+    fn eq(&self, other: &Health) -> bool {
+        other == self
+    }
+}
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ListBackupsResponse {
     pub backups: Vec<Backup>,
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ListClustersResponse {
     pub clusters: Vec<Cluster>,
 }
 
 /// The projection level of your database. Can be off, system or user
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub enum ProjectionLevel {
     Off,
@@ -175,9 +259,29 @@ impl std::fmt::Display for ProjectionLevel {
         }
     }
 }
+impl std::cmp::PartialEq<&str> for ProjectionLevel {
+    fn eq(&self, other: &&str) -> bool {
+        match self {
+            ProjectionLevel::Off => *other == "off",
+            ProjectionLevel::System => *other == "system",
+            ProjectionLevel::User => *other == "user",
+        }
+    }
+}
+impl std::cmp::PartialEq<ProjectionLevel> for &str {
+    fn eq(&self, other: &ProjectionLevel) -> bool {
+        other == self
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RestartClusterResponse {
+    pub id: String,
+}
 
 /// Either single-node or three-node-multi-zone
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Topology {
     #[serde(rename = "single-node")]
     SingleNode,
@@ -192,9 +296,25 @@ impl std::fmt::Display for Topology {
         }
     }
 }
+impl std::cmp::PartialEq<&str> for Topology {
+    fn eq(&self, other: &&str) -> bool {
+        match self {
+            Topology::SingleNode => *other == "single-node",
+            Topology::ThreeNodeMultiZone => *other == "three-node-multi-zone",
+        }
+    }
+}
+impl std::cmp::PartialEq<Topology> for &str {
+    fn eq(&self, other: &Topology) -> bool {
+        other == self
+    }
+}
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct UpdateClusterRequest {
-    pub description: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub protected: Option<bool>,
 }
